54523244bd66280e32f26a822a80dbbdab35adf8
Junting <342766475@qq.com>
2020-09-10 23:13:44 +0800

    完成 Tic Tac Toe 中版

--- src/main.js
+++ src/main.js
@@ -42,13 +42,89 @@ import { createElement, Component, render } from './ToyReact';
 
 
 
+// class Square extends Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+// class Board extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       squares: Array(9).fill(null),
+//     };
+//   }
+
+//   handleClick(i) {
+//     const squares = this.state.squares.slice();
+//     squares[i] = 'X';
+//     this.setState({squares: squares});
+//   }
+
+//   renderSquare(i) {
+//     return (
+//       <Square
+//         value={this.state.squares[i]}
+//         onClick={() => this.handleClick(i)}
+//       />
+//     );
+//   }
+
+//   render() {
+//     const status = 'Next player: X';
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// class Game extends Component {
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className="game-info">
+//           <div>{/* status */}</div>
+//           <ol>{/* TODO */}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// // ========================================
+
+// render(
+//   <Game />,
+//   document.getElementById('root')
+// );
+
 class Square extends Component {
-  render() {
+  render () {
     return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
+      <button className="square" onClick={this.props.onClick}>
         {this.props.value}
       </button>
     );
@@ -60,13 +136,20 @@ class Board extends Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -79,19 +162,31 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
@@ -120,3 +215,23 @@ render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
